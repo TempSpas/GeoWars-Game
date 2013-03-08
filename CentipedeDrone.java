40243cbc68c83e9079f61fec54aa0a883b0d7328
TempSpas <nicolaasmverbeek+code@gmail.com>
2013-03-08 13:35:31 -0500

    Create CentipedeDrone.java
    Drone that travels in a straight line up and down the grid leaving 
    behind TailDrones. Upon reaching an edge, moves to an adjacent row 
    and begins to move in the opposite north/south direction.

---
+++ CentipedeDrone.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+public class CentipedeDrone extends Drone
+{
+  public CentipedeDrone(ActorWorld w)
+	{
+		super(w, 100);
+		setColor(Color.RED);
+	}
+
+	public void act()
+	{
+		//super.act();
+		if(canMove())
+			move();
+		else
+			turn();
+	}
+
+	public void turn()
+	{
+		setDirection(getDirection() + Location.HALF_RIGHT);
+	}
+
+	public void move()
+	{
+		Grid<Actor> gr = getGrid();
+		if (gr == null)
+			return;
+		Location loc = getLocation();
+		Location next = loc.getAdjacentLocation(getDirection());
+		if (gr.isValid(next))
+			moveTo(next);
+		else
+			removeSelfFromGrid();
+		CentipedeTail tail = new CentipedeTail(world);
+		tail.putSelfInGrid(gr, loc);
+	}
+
+	public boolean canMove()
+	{
+		Grid<Actor> gr = getGrid();
+		if (gr == null)
+			return false;
+		Location loc = getLocation();
+		Location next = loc.getAdjacentLocation(getDirection());
+		if (!gr.isValid(next))
+			return false;
+		Actor neighbor = gr.get(next);
+		return (neighbor == null)/* || (neighbor instanceof Flower)*/;
+	}
+}
