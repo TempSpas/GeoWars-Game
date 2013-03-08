1633900f1a4edebe4ee770121084ac48580c95dc
TempSpas <nicolaasmverbeek+code@gmail.com>
2013-03-08 13:22:24 -0500

    Create LeaderboardEntry.java

---
+++ LeaderboardEntry.java
@@ -0,0 +1,51 @@
+public class LeaderboardEntry implements Comparable<LeaderboardEntry>
+{
+  private String initials;
+	private int score;
+	
+	public LeaderboardEntry()
+	{
+		initials = "";
+		score = 0;
+	}
+	
+	public LeaderboardEntry(String i, int s)
+	{
+		initials = i;
+		score = s;
+	}
+	
+	public int compareTo(LeaderboardEntry l)
+	{
+		if(this.getScore() < l.getScore())
+			return 1;
+		else if(this.getScore() > l.getScore())
+			return -1;
+		return 0;
+	}
+	
+	public String getInitials()
+	{
+		return initials;
+	}
+	
+	public void setInitials(String s)
+	{
+		initials = s;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public void setScore(int s)
+	{
+		score = s;
+	}
+	
+	public String toString()
+	{
+		return initials+"\t\t\t\t\t\t\t\t"+score;
+	}
+}
