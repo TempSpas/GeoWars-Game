da0604dfeac5feb17aef555f469d21e80d81d49c
TempSpas <nicolaasmverbeek+code@gmail.com>
2013-03-13 23:10:03 -0300

    Update PacifistWorld.java

--- PacifistWorld.java
+++ PacifistWorld.java
@@ -1,4 +1,14 @@
 public class PacifistWorld extends KeyWorld
 {
-  
+  private void enemySpawn(Location loc)
+  {
+		int chance = (int)(Math.random()*100+1)
+		if(chance <= 99)
+		{
+			int randEnemy = (int)(Math.random()*100+1)
+			if(randEnemy <= 80)
+				this.add(loc, new Drone(this));
+			else this.add(loc, new Gate());
+		}
+	}
 }
